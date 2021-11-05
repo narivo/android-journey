13fae2ba1f8011cbf1ec6344173aa47e3eaf6550
Andrianarivo David <87760993+narivo@users.noreply.github.com>
2021-11-05 16:07:03 +0300

    finished  creating dial dialog

--- KotlinCustomViews/app/src/main/java/com/example/kotlincustomviews/ColorDialView.kt
+++ KotlinCustomViews/app/src/main/java/com/example/kotlincustomviews/ColorDialView.kt
@@ -3,11 +3,13 @@ package com.example.kotlincustomviews
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
+import androidx.core.content.ContextCompat
 
 /**
  * TODO: document your custom view class.
@@ -22,37 +24,81 @@ class ColorDialView @JvmOverloads constructor(context: Context,
         Color.BLUE, Color.GREEN, Color.DKGRAY, Color.CYAN, Color.MAGENTA,
         Color.BLACK)
 
+    private var noColorDrawable: Drawable? = null
     private var dialDrawable: Drawable? = null
+    private val paint = Paint().also{
+        it.color = Color.BLUE
+        it.isAntiAlias = true
+
+    }
 
     private var dialDiameter = toDP(100)
+    private var extraPadding = toDP(30)
+    private var tickSize = toDP(10).toFloat()
+    private var angleBetweenColors = 0f
 
+    // Pre-computed padding values
+    private var totalLeftPadding = 0f
+    private var totalTopPadding = 0f
+    private var totalRightPadding = 0f
+    private var totalBottomPadding = 0f
+
+    // Pre-computed helper values
     private var horizontalSize = 0f
     private var verticalSize = 0f
 
-    //Pre-computed position values
+    // Pre-computed position values
+    private var tickPositionVertical = 0f
     private var centerHorizontal = 0f
     private var centerVertical = 0f
 
     init {
 
-        dialDrawable = context.getDrawable(R.drawable.ic_dial).also {
+        dialDrawable = ContextCompat.getDrawable(context, R.drawable.ic_dial).also {
             it?.bounds = getCenteredBounds(dialDiameter)
             it?.setTint(Color.DKGRAY)
         }
+        noColorDrawable = ContextCompat.getDrawable(context, R.drawable.ic_baseline_clear_24).also {
+            it?.bounds = getCenteredBounds(tickSize.toInt(), 2f)
+        }
+        colors.add(0, Color.TRANSPARENT)
+        angleBetweenColors = 360f/ colors.size
         refreshValues()
     }
 
     private fun refreshValues() {
-        //Compute helper values
-        this.horizontalSize = dialDiameter.toFloat()
-        this.verticalSize = dialDiameter.toFloat()
+        // Compute padding values
+        this.totalLeftPadding = (paddingLeft + extraPadding).toFloat()
+        this.totalRightPadding = (paddingRight + extraPadding).toFloat()
+        this.totalTopPadding = (paddingTop + extraPadding).toFloat()
+        this.totalBottomPadding = (paddingBottom + extraPadding).toFloat()
 
-        this.centerHorizontal = horizontalSize  / 2f
-        this.centerVertical = verticalSize / 2f
+        //Compute helper values
+        this.horizontalSize = paddingLeft + paddingRight + (extraPadding * 2) + dialDiameter.toFloat()
+        this.verticalSize = paddingTop + paddingBottom + (extraPadding * 2) + dialDiameter.toFloat()
+
+        // Compute position values
+        this.tickPositionVertical = paddingTop + extraPadding / 2f
+        this.centerHorizontal = totalLeftPadding + (horizontalSize - totalLeftPadding - totalRightPadding)  / 2f
+        this.centerVertical = totalTopPadding + (verticalSize - totalTopPadding - totalBottomPadding) / 2f
     }
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+        //super.onDraw(canvas)
+        val saveCount = canvas.save()
+        colors.forEachIndexed { index, color ->
+            if (index == 0) {
+                canvas.translate(centerHorizontal, tickPositionVertical)
+                noColorDrawable?.draw(canvas)
+                canvas.translate(-centerHorizontal, -tickPositionVertical)
+            } else {
+                paint.color = colors[index]
+                canvas.drawCircle(centerHorizontal, tickPositionVertical, tickSize, paint)
+
+            }
+            canvas.rotate(angleBetweenColors, centerHorizontal, centerVertical)
+        }
+        canvas.restoreToCount(saveCount)
 
         canvas.translate(centerHorizontal, centerVertical)
         dialDrawable?.draw(canvas)
